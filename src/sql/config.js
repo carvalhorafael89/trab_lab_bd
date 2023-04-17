ed0f06440d7fc392305b866b210782db17ea46bc
carvalhorafael89 <rafael_cidao@yahoo.com.br>
2023-04-17 09:30:29 -0300

    final

--- src/sql/config.js
+++ src/sql/config.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv/config'
 
 export const sqlConfig = {
-  user: process.env.DB_NAME,
-  password: process.env.DB_PWD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_NAME,
+  user: 'rafae',
+  password: '090313',
+  server: 'localhost\SQLEXPRESS',
+  database: 'bdfilmes.sql',
   port: 1433,
   options: {
       encrypt: true,
